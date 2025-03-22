fb5dd380ac2de4e85adbf61aad79e45cf161afe0
Jeremy Aza <jeremy.aza@sumamexico.com>
2025-03-22 02:00:05 -0500

    feat: initial structure

---
+++ deps_autocapture/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    id("maven-publish")
+}
+
+android {
+    namespace = "com.example.deps_autocapture"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 21
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            groupId = "com.example"
+            artifactId = "deps_autocapture"
+            version = "1.0.0"
+
+            fileTree("libs").forEach { file -> artifact(file) }
+        }
+    }
+}
+
+dependencies {}
